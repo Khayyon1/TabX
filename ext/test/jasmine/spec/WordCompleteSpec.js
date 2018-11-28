69dd8b7f66d950534d3d4a981d99d8bea7195db6
Khayyon1 <k_parker@coloradocollege.edu>
2018-11-28 11:42:19 -0700

    Added Test Cases to Get Sugestions and HTML folder for testing purposes

--- ext/test/jasmine/spec/WordCompleteSpec.js
+++ ext/test/jasmine/spec/WordCompleteSpec.js
@@ -4,10 +4,15 @@
 *     
 */    
 
-describe("Hello World", function(){ 
-   
-   it("should Return Hello world", function(){ 
-      expect(getSuggestions("hello")).toBe(true); 
-   }); 
+describe("Get Suggestions", function(){
+   it("should return non-empty suggestions list ", function(){
+      expect(getSuggestions("hello").length > 0).toBe(true);
+   });
+
+    it('should currently return a suggestion list of size 3', function () {
+       expect(getSuggestions("hello").length == 3).toBe(true);
+    });
+
 
 });
+

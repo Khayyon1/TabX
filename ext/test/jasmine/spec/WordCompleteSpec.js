1a05325eb3d50f0d0e58a2ac4eef8d582584d8e8
Miguel Guerrero <miguel_angel_g@live.com>
2018-11-28 11:54:26 -0700

    testing out some refactoring techs for Jasmine

--- ext/test/jasmine/spec/WordCompleteSpec.js
+++ ext/test/jasmine/spec/WordCompleteSpec.js
@@ -1,18 +1,19 @@
-/*            
-* This is the file which will call our java script file that need to be tested. 
-* Each describe block is equivalent to one test case    
-*     
-*/    
+/*
+* This is the file which will call our java script file that need to be tested.
+* Each describe block is equivalent to one test case
+*
+*/
 
-describe("Get Suggestions", function(){
-   it("should return non-empty suggestions list ", function(){
+describe("Get Suggestions", suggestionsTestSuite);
+
+function suggestionsTestSuite()
+{
+   it("should return non-empty suggestions list", function()
+   {
       expect(getSuggestions("hello").length > 0).toBe(true);
    });
 
-    it('should currently return a suggestion list of size 3', function () {
-       expect(getSuggestions("hello").length == 3).toBe(true);
-    });
-
-
-});
-
+   it("shoud return an empty suggestions list for a nonsense word", function(){
+      expect(getSuggestions("fjuernf").length == 0).toBe(true);
+   });
+}

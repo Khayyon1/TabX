c213a0881b69a765c8f67267aa344c047cafcccf
m_davaadorj@coloradocollege.edu <dmishig@gmail.com>
2018-12-05 11:01:09 -0700

    feat: OOD

--- ext/src/lib/wordpredmchains/app.js
+++ ext/src/lib/wordpredmchains/app.js
@@ -1,42 +1,64 @@
-var nlp = require('./nlp_compromise.min')
+const nlp = require('./nlp_compromise.min')
 const txt = require('./text')
 
-var tokens = [];
-
-function createTokens() {
-    var nlp_text = nlp.text(txt);
-    var terms = nlp_text.terms();
-    for (var i = 0; i < terms.length; i++) {
-        tokens.push(terms[i].text);
+class WordPrediction{
+    constructor(){
+        this.tokens = [];
     }
-}
-
-function chooseStartingToken() {
-    var index = Math.floor(Math.random() * tokens.length);
-    return tokens[index];
-}
-
-function findNextWord(currentWord) {
-    var nextWords = [];
-    for (var w = 0; w < tokens.length - 1; w++) {
-        if (tokens[w] == currentWord) {
-            nextWords.push(tokens[w + 1]);
+    createTokens() {
+        this.nlp_text = nlp.text(txt);
+        var terms = this.nlp_text.terms();
+        for (var i = 0; i < terms.length; i++) {
+            this.tokens.push(terms[i].text);
         }
     }
-    console.log(currentWord, nextWords)
-    var word = nextWords[Math.floor(Math.random() * nextWords.length)]; // choose a random next word
-    return word;
-}
-
-
-function start() {
-    createTokens();
-    var currentWord = chooseStartingToken();
-    var sentence = currentWord + " ";
-    while (currentWord.indexOf(".") < 0) { // while we haven't found a period
-        currentWord = findNextWord(currentWord);
-        sentence += currentWord + " ";
+    chooseStartingToken() {
+        var index = Math.floor(Math.random() * this.tokens.length);
+        return this.tokens[index];
     }
-    console.log(sentence);
+    findNextWordL(currentWord) {
+        var nextWords = [];
+        for (var w = 0; w < this.tokens.length - 1; w++) {
+            if (this.tokens[w] == currentWord) {
+                nextWords.push(this.tokens[w + 1]);
+            }
+            if (nextWords.length == 4) break;
+        }
+        var word = nextWords[Math.floor(Math.random() * nextWords.length)]; // choose a random next word
+        return word;
+    }
+    findNextWord(currentWord) {
+        var nextWords = [];
+        for (var w = 0; w < this.tokens.length - 1; w++) {
+            if (this.tokens[w] == currentWord) {
+                this.addPossibleNextWord(nextWords, this.tokens[w + 1]);
+            }
+            if (nextWords.length == 4) break;
+        }
+        return nextWords;
+    }
+    addPossibleNextWord(nextWords, word){
+        nextWords.forEach((n_word) => {
+            if (n_word == word) return;
+        });
+        nextWords.push(word);
+    }
+    sentenceGeneration() {
+        this.createTokens();
+        var currentWord = this.chooseStartingToken();
+        var sentence = currentWord + " ";
+        while (currentWord.indexOf(".") < 0) { // while we haven't found a period
+            currentWord = this.findNextWord(currentWord);
+            sentence += currentWord + " ";
+        }
+        console.log(sentence);
+    }
+    predict(seed){
+        const next_words = this.findNextWord(seed);
+        console.log(next_words);
+    }
 }
-start();
+
+const model = new WordPrediction();
+model.createTokens();
+model.predict("you");

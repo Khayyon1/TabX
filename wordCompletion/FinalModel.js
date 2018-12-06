98f856965dcfb1e5b4f9faa08e76ae0bd5a6496a
Russell <icestar.ru@gmail.com>
2018-12-05 18:46:26 -0700

    added alternative suggestions

--- wordCompletion/FinalModel.js
+++ wordCompletion/FinalModel.js
@@ -138,6 +138,127 @@ Trie.getSuggestion = function(key){
 
     return finalArray
 }
+Trie.getSuggestion = function(key){
+
+    this.add(key, false)
+    key = key.toLowerCase();
+    keyHold = JSON.parse(JSON.stringify(key));
+    var total = 3;
+    var finalArray = new Array();
+    var curNode = this.head;
+  	var curChar = key.slice(0,1);
+    var cQueue = []
+    key = key.slice(1);
+
+    //Node Stack to keep track
+    var nodeStack = []
+    //findNode
+
+    while(typeof curNode.children[curChar] !== "undefined" && curChar.length > 0){
+        nodeStack.push(curNode)
+        //Traverse 1 layer down the tree
+        curNode = curNode.children[curChar];
+        //add next character
+        curChar = key.slice(0,1);
+        //remove 1 character from key
+        key = key.slice(1);
+    }
+    nodeStack.push(curNode)
+    //This checks Stores each letter in prediction word
+    var cStack = []
+    for (var i in keyHold){
+        cStack.push(keyHold[i])
+    }
+    cStack.push("");
+    while(nodeStack.length > 0 && total > 0){
+        tempArrayChild = this.getChildren(finalArray,total, nodeStack.pop(), cStack.pop() );
+        finalArray = tempArrayChild[0]
+        total = tempArrayChild[1]
+    }
+
+    return finalArray
+}
+Trie.getCorrectionSuggestions = function(key){
+    this.add(key, false)
+    key = key.toLowerCase();
+    keyHold = JSON.parse(JSON.stringify(key));
+    var total = 3;
+    var finalArray = new Array();
+    var curNode = this.head;
+  	var curChar = key.slice(0,1);
+    var cQueue = []
+    key = key.slice(1);
+
+    //Node Stack to keep track
+    var nodeStack = []
+    //findNode
+
+    while(typeof curNode.children[curChar] !== "undefined" && curChar.length > 0){
+        nodeStack.push(curNode)
+        //Traverse 1 layer down the tree
+        curNode = curNode.children[curChar];
+        //add next character
+        curChar = key.slice(0,1);
+        //remove 1 character from key
+        key = key.slice(1);
+    }
+
+    //This checks Stores each letter in prediction word
+    var cStack = []
+    for (var i in keyHold){
+        cStack.push(keyHold[i])
+    }
+    //cStack.push("");
+    //nodeStack.pop();
+    while(nodeStack.length > 0 && total > 0){
+        tempArrayChild = this.getChildren(finalArray,total, nodeStack.pop(), cStack.pop() );
+        finalArray = tempArrayChild[0]
+        total = tempArrayChild[1]
+    }
+
+    return finalArray
+}
+
+Trie.getWordCompletion = function(key){
+    this.add(key, false)
+    key = key.toLowerCase();
+    keyHold = JSON.parse(JSON.stringify(key));
+    var total = 3;
+    var finalArray = new Array();
+    var curNode = this.head;
+    var curChar = key.slice(0,1);
+    var cQueue = []
+    key = key.slice(1);
+
+    //Node Stack to keep track
+    var nodeStack = []
+    //findNode
+
+    while(typeof curNode.children[curChar] !== "undefined" && curChar.length > 0){
+        nodeStack.push(curNode)
+        //Traverse 1 layer down the tree
+        curNode = curNode.children[curChar];
+        //add next character
+        curChar = key.slice(0,1);
+        //remove 1 character from key
+        key = key.slice(1);
+    }
+    nodeStack.push(curNode)
+    //This checks Stores each letter in prediction word
+    var cStack = []
+    for (var i in keyHold){
+        cStack.push(keyHold[i])
+    }
+    cStack.push("");
+    tempArrayChild = this.getChildren(finalArray,total, nodeStack.pop(), cStack.pop());
+    finalArray = tempArrayChild[0]
+    total = tempArrayChild[1]
+
+
+    return finalArray
+}
+
+
 Trie.remove = function(key) {
     key = key.toLowerCase();
 	var d = this.search(key);
@@ -145,3 +266,8 @@ Trie.remove = function(key) {
     		removeH(this.head, key, d);
     	}
 }
+
+// console.log(Trie.getSuggestion("the"));
+// console.log(Trie.getWordCompletion("the"));
+//console.log(Trie.getCorrectionSuggestions("the"));
+module.exports = Trie

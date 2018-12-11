b566d7195a424b131df56fe554f84302667fee94
Nicole <nwoch101@gmail.com>
2018-12-11 15:56:27 -0700

    Replace old markov files

--- test.js
+++
@@ -1,9 +0,0 @@
-var test = require('./word-prediction');
-
-var sigs = test.blogsModel.predictNextWord("that");
-console.log(sigs);
-//library: [ 'out', 'on', 'a' ], slower
-//brown: [ 'if', 'line' ] for print, slowest
-//news: out
-//blogs: [ 'all', 'projects', 'ad' ], closest to everyday speech
-//fake: none

3c24b93008446c59ceb4c6dd4db4f62771869246
Mishiggy <m_davaadorj@coloradocollege.edu>
2018-12-11 17:20:28 -0700

    chore: turn down autofill for now

--- ext/src/autofill.js
+++ ext/src/autofill.js
@@ -9,18 +9,18 @@ class Autofill
         this.completion = val;
     }
     fill(el, suggestion) {
-        if (suggestion != undefined && this.active) {
-            if (this.completion) {
-                const prefix = el.value.split(" ").pop();
-                suggestion = suggestion.substring(prefix.length);
-            }
-            this.lastWord = suggestion;
-            el.value += suggestion;
-            el.selectionStart = el.value.length - suggestion.length;
-            el.selectionEnd = el.value.length;
-        }else{
-            this.lastWord = '';
-        }
+        // if (suggestion != undefined && this.active) {
+        //     if (this.completion) {
+        //         const prefix = el.value.split(" ").pop();
+        //         suggestion = suggestion.substring(prefix.length);
+        //     }
+        //     this.lastWord = suggestion;
+        //     el.value += suggestion;
+        //     el.selectionStart = el.value.length - suggestion.length;
+        //     el.selectionEnd = el.value.length;
+        // }else{
+        //     this.lastWord = '';
+        // }
     }
     shortcutPressed(el){
         el.value = el.value.substring(0, el.value.length - this.lastWord.length);

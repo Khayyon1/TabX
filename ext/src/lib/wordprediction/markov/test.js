d1bf41fd59c53483cefe7630ed68983e867d2cdd
Nicole <nwoch101@gmail.com>
2018-12-14 13:44:41 -0700

    Merge updates

--- ext/src/lib/wordprediction/markov/test.js
+++
@@ -1,6 +0,0 @@
-var test = require('./word-prediction');
-
-console.log(test.blogsModelLarge.predictNextWord("sfgeh can't I "));
-var str = "FsGH'vF"
-console.log(str[0] + str.slice(1).toLowerCase());
-

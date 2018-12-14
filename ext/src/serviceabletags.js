d1a00c6c6d7b20352a8a752729ed7465ae5a9781
Miguel Guerrero <miguel_angel_g@live.com>
2018-12-13 19:50:05 -0700

    getting caret ready to be tested

--- ext/src/serviceabletags.js
+++ ext/src/serviceabletags.js
@@ -26,6 +26,50 @@ function activeElementIsServiceable()
     return false;
 }
 
+function caretAndText(parentEditableDiv, targetDiv)
+{
+   if(parentEditableDiv == targetDiv)
+   {
+      return window.getSelection().anchorOffset;
+   }
+
+   let text = getText(parentEditableDiv, targetDiv);
+   let caret = (text.length + window.getSelection().anchorOffset)
+
+   return {text: test, caret: caret};
+}
+
+function getTextInEditableDiv(root, target)
+{
+   let cur = target;
+   let text = "";
+   while(cur != root)
+   {
+      while(cur.previousSibling != null)
+      {
+         cur = cur.previousSibling;
+         text = cur.textContent + text;
+         if(["P", "DIV"].includes(cur.tagName))
+         {
+            text = " " + text;
+         }
+      }
+
+      //Edge case if the parent node is a div or p tag
+      //since the text associated with the parent is its child,
+      //which cannot be detected with the above check in the while
+      //loop
+      cur = cur.parentNode;
+      if(["P", "DIV"].includes(cur.tagName))
+      {
+         text = " " + text;
+      }
+   }
+
+   return text;
+}
+
+
 module.exports = {
     getServicableElements: getServicableElements,
     activeElementIsServiceable: activeElementIsServiceable

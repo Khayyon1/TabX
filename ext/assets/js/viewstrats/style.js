88691fd6352328e57e67f6146aa777fb123316be
Mishiggy <m_davaadorj@coloradocollege.edu>
2018-12-16 13:33:32 -0700

    feat: style

--- ext/assets/js/viewstrats/style.js
+++ ext/assets/js/viewstrats/style.js
@@ -56,60 +56,6 @@ const Style = class
             element.style.fontWeight = this.settings.fontstyle.toLowerCase();
         }
     }
-    calcSize(text, options = {}) {
-
-        const cacheKey = JSON.stringify({ text: text, options: options })
-
-        if (this.cache[cacheKey]) {
-            return this.cache[cacheKey]
-        }
-
-        // prepare options
-        options.font = options.font || 'Times'
-        options.fontSize = options.fontSize || '16px'
-        options.fontWeight = options.fontWeight || 'normal'
-        options.lineHeight = options.lineHeight || 'normal'
-        options.width = options.width || 'auto'
-        options.wordBreak = options.wordBreak || 'normal'
-
-        const element = this.createDummyElement(text, options)
-
-        const size = {
-            width: element.offsetWidth,
-            height: element.offsetHeight,
-        }
-
-        this.destroyElement(element)
-
-        this.cache[cacheKey] = size
-
-        return size
-    }
-    destroyElement(element) {
-        element.parentNode.removeChild(element)
-    }
-    createDummyElement(text, options) {
-        const element = document.createElement('div')
-        const textNode = document.createTextNode(text)
-
-        element.appendChild(textNode)
-
-        element.style.fontFamily = options.font
-        element.style.fontSize = options.fontSize
-        element.style.fontWeight = options.fontWeight
-        element.style.lineHeight = options.lineHeight
-        element.style.position = 'absolute'
-        element.style.visibility = 'hidden'
-        element.style.left = '-999px'
-        element.style.top = '-999px'
-        element.style.width = options.width
-        element.style.height = 'auto'
-        element.style.wordBreak = options.wordBreak
-
-        document.body.appendChild(element)
-
-        return element
-    }
 }
 
 module.exports = Style;
--- ext/chrome/scripts/tabx.js
+++ ext/chrome/scripts/tabx.js
@@ -659,7 +659,6 @@ const Style = class
         const caret = getCaretCoordinates(textInputBox, textInputBox.selectionStart);
         this.offset_y = window.getComputedStyle(textInputBox, "").fontSize;
         this.offset_y = this.pxToInt(this.offset_y) - textInputBox.scrollTop;
-        console.log('SCROOLLL',textInputBox.scrollTop);
         element.style.top = (rect.top + caret.top + this.offset_y).toString() + 'px';
         element.style.left = (rect.left + caret.left).toString() + 'px';
     }
@@ -698,60 +697,6 @@ const Style = class
             element.style.color = this.settings.fontcolor;
             element.style.fontWeight = this.settings.fontstyle.toLowerCase();
         }
-    }
-    calcSize(text, options = {}) {
-
-        const cacheKey = JSON.stringify({ text: text, options: options })
-
-        if (this.cache[cacheKey]) {
-            return this.cache[cacheKey]
-        }
-
-        // prepare options
-        options.font = options.font || 'Times'
-        options.fontSize = options.fontSize || '16px'
-        options.fontWeight = options.fontWeight || 'normal'
-        options.lineHeight = options.lineHeight || 'normal'
-        options.width = options.width || 'auto'
-        options.wordBreak = options.wordBreak || 'normal'
-
-        const element = this.createDummyElement(text, options)
-
-        const size = {
-            width: element.offsetWidth,
-            height: element.offsetHeight,
-        }
-
-        this.destroyElement(element)
-
-        this.cache[cacheKey] = size
-
-        return size
-    }
-    destroyElement(element) {
-        element.parentNode.removeChild(element)
-    }
-    createDummyElement(text, options) {
-        const element = document.createElement('div')
-        const textNode = document.createTextNode(text)
-
-        element.appendChild(textNode)
-
-        element.style.fontFamily = options.font
-        element.style.fontSize = options.fontSize
-        element.style.fontWeight = options.fontWeight
-        element.style.lineHeight = options.lineHeight
-        element.style.position = 'absolute'
-        element.style.visibility = 'hidden'
-        element.style.left = '-999px'
-        element.style.top = '-999px'
-        element.style.width = options.width
-        element.style.height = 'auto'
-        element.style.wordBreak = options.wordBreak
-
-        document.body.appendChild(element)
-
-        return element
     }
 }
 

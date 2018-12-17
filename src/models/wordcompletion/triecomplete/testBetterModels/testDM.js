92ee1e83b57fb23bd6c24008e27f553c10e9c4a7
Russell <icestar.ru@gmail.com>
2018-12-16 22:11:50 -0700

    updated model to best

--- src/models/wordcompletion/triecomplete/testBetterModels/testDM.js
+++ src/models/wordcompletion/triecomplete/testBetterModels/testDM.js
@@ -8,31 +8,59 @@ var am = train.addMidW;
 var ae = train.addEndW;
 var swg = train.swapGenW;
 var sbg = train.subGenW;
+var db = train.delBegW;
+var dm = train.delMidW;
+var de = train.delEndW;
 
 
+dmod = train.myTrain
 //Prelim tests
-expect(train.distance("","")).to.be(0)
-expect(train.distance("a", "a")).to.be(0);
-expect(train.distance("abc", "abc")).to.be(0);
-expect(train.distance(" ", " ")).to.be(0);
-expect(train.distance("a", "ab")).to.be(ae);
 
 
-expect(train.distance("a", "ba")).to.be(ab);
+expect(dmod.distance("","")).to.be(0)
+expect(dmod.distance("a", "a")).to.be(0);
+
+expect(dmod.distance("abc", "abc")).to.be(0);
+expect(dmod.distance(" ", " ")).to.be(0);
+
+//Test for Add
+expect(dmod.distance("a", "ab")).to.be(ae);
+expect(dmod.distance("a", "ba")).to.be(ab);
+expect(dmod.distance("ac", "abc")).to.be(am);
+expect(dmod.distance("a", "abc")).to.be(ae*2);
+expect(dmod.distance("b", "abc")).to.be(ab + ae);
+expect(dmod.distance("ac", "abcd")).to.be(am + ae);
+expect(dmod.distance("bd", "abcde")).to.be(ab + am + ae);
+
+//Tests For Delete
 
 
+expect(dmod.distance("ab", "a")).to.be(de);
+expect(dmod.distance("ba", "a")).to.be(db);
+expect(dmod.distance("abc", "ac")).to.be(dm);
+expect(dmod.distance("abc", "a")).to.be(de*2);
+expect(dmod.distance("abc", "b")).to.be(db + de);
+expect(dmod.distance("abcd", "ac")).to.be(dm + de);
+expect(dmod.distance("abcde", "bd")).to.be(db + dm + de);
 
 
-expect(train.distance("ac", "abc")).to.be(am);
-expect(train.distance("a", "abc")).to.be(ae*2);
+expect(dmod.distance("a", "b")).to.be(sbg);
+expect(dmod.distance("ab", "ac")).to.be(sbg);
 
-expect(train.distance("b", "abc")).to.be(ab + ae);
-expect(train.distance("ac", "abcd")).to.be(am + ae);
-expect(train.distance("bd", "abcde")).to.be(ab + am + ae);
+expect(dmod.distance("abc", "zyx")).to.be(sbg*3);
+expect(dmod.distance("ab", "ba")).to.be(swg);
 
-expect(train.distance("a", "b")).to.be(sbg);
 
-expect(train.distance("ab", "ac")).to.be(sbg);
-expect(train.distance("abc", "zyx")).to.be(sbg*3);
+expect(dmod.distance("a", "az")).to.be(ae);
+dmod.changeValue("addEnd", 0.1 ,"z");
+expect(dmod.distance("a", "az")).to.be(0.1);
 
-expect(train.distance("ab", "ba")).to.be(swg);
+expect(dmod.distance("ac", "abc")).to.be(am);
+dmod.changeValue("addMid", 0.2 ,"b");
+expect(dmod.distance("ac", "abc")).to.be(0.2);
+
+expect(dmod.distance("bc", "ybc")).to.be(ab);
+dmod.changeValue("addBeg", 0.8 ,"y");
+expect(dmod.distance("bc", "ybc")).to.be(0.8);
+
+//expect(dmod.distance("abc", "ybc")).to.be(ab);
--- src/models/wordcompletion/triecomplete/testBetterModels/understandDM.js
+++ src/models/wordcompletion/triecomplete/testBetterModels/understandDM.js
@@ -2,35 +2,148 @@
 
 'use strict';
 
-const addBegW = 0.6;
-const addEndW = 0.2;
-const addMidW = 0.3;
-const subGenW = 1.3;
-const swapGenW = 1;
+
+///Sub Should Never Be more than delete + Add
+const addBegW = 0.9;
+const addMidW = 0.7;
+const addEndW = 0.5;
+
+const delBegW = 0.8;
+const delMidW = 0.6;
+const delEndW = 0.4;
+
+const subGenW = 1.1;
+const swapGenW = 0.7;
+const typeArray = ["addBeg", "addMid", "addEnd",
+        "delBeg", "delMid", "delEnd",
+        "subGen", "swapGen"]
 //export addW, addEndW, addMidW, subW, swapW};
 
 // Initialize the Matrix of Values
 //Fill matrix with outside edge of values
-function initMatrix(write, guess) {
+
+//TODO make customizable weights.
+
+function modelTrain(style = 'default', addBeg = addBegW, addMid = addMidW,
+ addEnd = addEndW, delBeg = delBegW,
+ delMid = delMidW, delEnd = delEndW,
+ subGen = subGenW, swapGen = swapGenW){
+
+     //TODO deal with numbers
+    this.buildModel = function(arrayVal = [addBeg, addMid, addEnd,
+            delBeg, delMid, delEnd,
+            subGen, swapGen]){
+        let charArray = [" ", "-", "'",];
+        for(let i = 0; i < 27; i++){
+        //Iterate over every character in english alphabet
+            charArray.push(String.fromCharCode(97 + i));
+        }
+        var values = {};
+        for(let i = 0; i < typeArray.length; i++){
+            values[typeArray[i]] = {}
+            if (typeArray[i] === 'swapGen' || typeArray[i] === 'subGen'){
+                for(let j = 0; j < charArray.length; j++){
+                    let tempDict = {}
+                    values[typeArray[i]][charArray[j]] = tempDict
+                    for (let k = 0; k < charArray.length; k++){
+                        if (charArray[k] !== charArray[j]){
+                            values[typeArray[i]][charArray[j]][charArray[k]] = arrayVal[i];
+                        }
+                    }
+                }
+            }
+            else{
+                for(let j = 0; j < charArray.length; j++){
+                    values[typeArray[i]][charArray[j]] = arrayVal[i]
+                }
+            }
+        }
+
+        return values
+    }
+
+    if (style === 'default' || style === 'custom'){
+        this.model = this.buildModel([addBeg, addMid, addEnd,
+                delBeg, delMid, delEnd,
+                subGen, swapGen]);
+    }
+
+    else if (style === 'even'){
+        this.model = this.buildModel(arrayVal = [1, 1, 1,
+                1, 1, 1,
+                1, 1]);
+    }
+    // console.log("model____")
+    // console.log(this.model['addEnd']['a']);
+    // console.log("Model____")
+}
+
+
+modelTrain.prototype.changeValue = function(type = 'swapGen', newValue, value1, value2){
+    if (value1 !== undefined && value2 !== undefined){
+        if(type === 'swapGen' || type === 'subGen'){
+            if(this.model[type][value1][value2] !== undefined){
+                this.model[type][value1][value2] = newValue;
+            }
+            else{
+                throw value1 + " or " + value2 + "Are not valid characters";
+            }
+        }
+        else{
+            throw "Please use the proper number of values";
+        }
+    }
+    else if (value1 !== undefined){
+
+        if(this.model[type][value1] !== undefined){
+            this.model[type][value1] = newValue;
+        }
+        else{
+            throw value1 + " is not valid characters";
+        }
+    }
+    else{
+        throw "please insert all values";
+    }
+
+
+
+    //this.model[type][]
+}
+         //Also need to iterate over space and Dash
+
+
+modelTrain.prototype.initMatrix = function(write, guess) {
     if (undefined == write || undefined == guess) {
         return null;
     }
+    // var beg = 100
+    // var end = 100
+    // //Rough Fix:
+    //
+    // for(let q = 0; q < write.length; q++){
+    //     for(let r = 0; r < guess.length; r++){
+    //         if (write[q] === guess[r]){
+    //             beg = r;
+    //             q = write.length + 1;
+    //         }
+    //     }
+    //
+    // }
 
-    let d = [];
-    for (let i = 0; i <= write.length; i++) {
+    let d = [[0]];
+    for (let i = 1; i <= write.length; i++) {
         d[i] = [];
-        d[i][0] = i;
+        d[i][0] = d[i-1][0]+this.model['delBeg'][write[i-1]];
     }
-    for (let j = 0; j <= guess.length; j++) {
-        d[0][j] = j;
+    for (let j = 1; j <= guess.length; j++) {
+        d[0][j] = d[0][j-1]+this.model['addBeg'][guess[j-1]];
     }
     return d;
 }
 
 //Get Distance between to strings
-function distance(write, guess) {
-
-
+modelTrain.prototype.distance = function(write, guess) {
     //if either value is not a string return undefined
     if (undefined == write || undefined == guess || 'string' !== typeof write
             || 'string' !== typeof guess) {
@@ -38,7 +151,7 @@ function distance(write, guess) {
     }
 
     //create Matrix
-    let d = initMatrix(write, guess);
+    let d = this.initMatrix(write, guess);
 
     //if both string length 0, return -1
     if (null === d) {
@@ -52,20 +165,37 @@ function distance(write, guess) {
     let totalEqual = 0;
     for (var guessIdx = 1; guessIdx <= guess.length; guessIdx++) {
         let cost;
-        let addTo;
+        let currAdd;
+        let currDel;
         //let addToWrite;
         for (let writeIdx = 1; writeIdx <= write.length; writeIdx++) {
-            if (totalEqual >= write.length){
-                addTo = addEndW;
+            //if no Similarities occur between write and guess
+            if(totalEqual === 0){
+                currAdd = 'addBeg';
+                currDel = 'delBeg';
             }
+            //if few similarities occur between write and guess
             else if(totalEqual > 0 && totalEqual < write.length){
-                addTo = addMidW;
+                currAdd = 'addMid';
+                currDel = 'delMid';
             }
-            else if(totalEqual === 0){
-                addTo =  addBegW
+            //if write is entirely contained within guess
+            else if (totalEqual == write.length){
+                currAdd = 'addEnd';
+                currDel = 'delEnd';
             }
             else{
-                throw "Something is wrong is total Equal"
+                throw "Something is wrong is total Equal" + totalEqual
+            }
+            if(totalEqual > 0 && totalEqual < guess.length){
+                currDel = 'delMid';
+            }
+            //if write is entirely contained within guess
+            else if (totalEqual === guess.length){
+                currDel = 'delEnd';
+            }
+            else if (totalEqual > guess.length){
+                throw "Something is wrong is total Equal" + totalEqual
             }
 
             //Do we Need to Do a substitution
@@ -75,18 +205,33 @@ function distance(write, guess) {
             }
 
             else {
-                cost = subGenW;
+                // console.log(write.charAt(writeIdx - 1));
+                // console.log(guess.charAt(guessIdx - 1));
+                // console.log(this.model['subGen'][write.charAt(writeIdx - 1)])
+                cost = this.model['subGen'][write.charAt(writeIdx - 1)][guess.charAt(guessIdx - 1)];
+                // console.log(cost);
             }
 
             //Min of one more  +1 on either
             //min of One fewer + both.
+            if (typeof this.model[currDel][write[writeIdx -1]] !== 'number'){
+                // console.log(currDel);
+                // console.log(this.model[currDel]);
+                throw "error"
+            }
+            if (typeof this.model[currAdd][guess[guessIdx -1]] !== 'number'){
+                // console.log(this.model[currAdd][write[guessIdx -1]]);
+                throw "error"
+            }
+
             d[writeIdx][guessIdx] = Math.min.apply(null, [
                 //Addition
-                d[writeIdx - 1][guessIdx] + addTo,
-                d[writeIdx][guessIdx - 1] + addTo,
+                d[writeIdx - 1][guessIdx] + this.model[currDel][write[writeIdx -1]],
+                d[writeIdx][guessIdx - 1] + this.model[currAdd][guess[guessIdx -1]],
                 //Substitution
                 d[writeIdx - 1][guessIdx - 1] + cost
             ]);
+            //console.log("cost " +cost)
 
             //Damerau Check
             if (writeIdx > 1 && guessIdx > 1 && write[writeIdx - 1] === guess[guessIdx - 2]
@@ -95,12 +240,15 @@ function distance(write, guess) {
                 //and two prior + cost
                 d[writeIdx][guessIdx] = Math.min.apply(null, [
                     d[writeIdx][guessIdx],
-                    d[writeIdx - 2][guessIdx - 2] + swapGenW
+                    d[writeIdx - 2][guessIdx - 2] + this.model['swapGen'][write[writeIdx - 1]][guess[guessIdx - 1]]
                 ]);
                 totalEqual += 2;
             }
+            //console.log(d[writeIdx][guessIdx])
         }
+
     }
+    //console.log(d)
     return d[write.length][guess.length];
 }
 
@@ -140,15 +288,20 @@ function minDistanceProm(write, list) {
         }
     });
 }
+var myTrain = new modelTrain();
 module.exports = {
     addBegW: addBegW,
     addEndW: addEndW,
     addMidW: addMidW,
     subGenW: subGenW,
     swapGenW: swapGenW,
-
-    distance: distance
+    delBegW : delBegW,
+    delMidW : delMidW,
+    delEndW : delEndW,
+    myTrain: myTrain
 }
+
+
 exports.distanceProm = distanceProm;
-exports.distance = distance;
+//exports.distance = distance;
 exports.minDistanceProm = minDistanceProm;

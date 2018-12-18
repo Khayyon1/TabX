5fa4421910328995070932cc6dea8c188aa97401
Miguel Guerrero <miguel_angel_g@live.com>
2018-12-18 12:10:28 -0700

    default table view for tabx on instantiation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 const TabX = require('./tabx');
-const FixedView = require("./assets/js/viewstrats/fixedstrat");
 const TableView = require("./assets/js/viewstrats/tablestrat-ui");
 const applySettings = require("./browserspec/settings");
 const bgmodels = require("./models/messenger-models");
@@ -10,7 +9,6 @@ const abbrevExpansionModel = require("./models/abbreviationexpansion/abbreviatio
 //const mocknext = require("./models/mock/nextword_mock")
 //const mockcomp = require("./models/mock/wordcomplete_mock")
 
-let fixedDisplay = new FixedView(document);
 let tableDisplay = new TableView(document);
 
 let tabx = new TabX(bgmodels.WordCompleteModel,
